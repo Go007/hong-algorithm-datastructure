10cacbce82d750bf86026fe84928e050f31ceb56
王宏 <wanghong3@dafycredit.com>
2019-01-05 11:18:10 +0800

    希尔排序

---
+++ src/main/java/com/hong/sort/ShellSort.java
@@ -0,0 +1,35 @@
+package com.hong.sort;
+
+/**
+ * <br>希尔排序</br>
+ * 缩小增量排序
+ */
+public class ShellSort {
+
+    public static void sort(int[] a) {
+        int d = a.length;
+        while (true) {
+            d = d / 2;
+            for (int x = 0; x < d; x++) {
+                for (int i = x + d; i < a.length; i = i + d) {
+                    int temp = a[i];
+                    int j;
+                    for (j = i - d; j >= 0 && a[j] > temp; j = j - d) {
+                        a[j + d] = a[j];
+                    }
+                    a[j + d] = temp;
+                }
+            }
+            if (d == 1) {
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int N = 20000;
+        int[] arr = SortTestHelper.generateRandomArray(N, 1, 100000);
+        SortTestHelper.testSort("com.hong.sort.ShellSort", arr);
+    }
+
+}
