37bf65bda9401542d8a47194cde4885192b4ff0a
王宏 <wanghong3@dafycredit.com>
2019-01-10 17:05:48 +0800

    三路快速排序

---
+++ src/main/java/com/hong/sort/QuickSort3Ways.java
@@ -0,0 +1,66 @@
+package com.hong.sort;
+
+/**
+ * <br>三路快速排序法</br>
+ */
+public class QuickSort3Ways {
+
+    private static void swap(int[] arr, int i, int j) {
+        int t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+    }
+
+
+    public static void sort(int[] arr) {
+        int n = arr.length;
+        sort(arr, 0, n - 1);
+    }
+
+    /**
+     * 递归使用快速排序,对arr[l...r]的范围进行排序
+     *
+     * @param arr
+     */
+    public static void sort(int[] arr, int l, int r) {
+        // 对于小规模数组, 使用插入排序
+        if (r - l <= 15) {
+            InsertionSort.sort(arr, l, r);
+            return;
+        }
+
+        // 随机在arr[l...r]的范围中, 选择一个数值作为标定点pivot
+        swap(arr, l, (int) (Math.random() * (r - l + 1)) + l);
+        int v = arr[l];
+        int lt = l; // arr[l+1...lt] < v
+        int gt = r + 1; // arr[gt...r] > v
+        int i = l + 1;
+
+        while (i < gt) {
+            if (arr[i] < v) {
+                swap(arr, i, lt + 1);
+                i++;
+                lt++;
+            } else if (arr[i] > v) {
+                swap(arr, i, gt - 1);
+                gt--;
+            } else { // arr[i] == v
+                i++;
+            }
+        }
+
+        swap(arr, l, lt);
+
+        sort(arr, l, lt - 1);
+        sort(arr, gt, r);
+
+    }
+
+    public static void main(String[] args) {
+        // Quick Sort也是一个O(nlogn)复杂度的算法
+        // 可以在1秒之内轻松处理100万数量级的数据
+        int N = 10000;
+        int[] arr = SortTestHelper.generateRandomArray(N, 1, 100000);
+        SortTestHelper.testSort("com.hong.sort.QuickSort3Ways", arr);
+    }
+}
