5bbffc1914c8abbdff6eb155c4174957d0b5b532
macrowang <macrowang@lexin.com>
2019-03-06 11:51:42 +0800

    单链表反转

--- src/main/java/com/hong/linked/ReverseLinkedList.java
+++ src/main/java/com/hong/linked/ReverseLinkedList.java
@@ -15,7 +15,12 @@ public class ReverseLinkedList<T> {
         Node<T> next;
 
         Node(T value) {
+            this(value, null);
+        }
+
+        Node(T value, Node<T> next) {
             this.value = value;
+            this.next = next;
         }
     }
 
@@ -119,33 +124,66 @@ public class ReverseLinkedList<T> {
     public static void main(String[] args) {
         ReverseLinkedList<Integer> nodeList = new ReverseLinkedList<>(1, 2, 3, 4, 5);
         System.out.println(nodeList);
-        System.out.println(nodeList.size());
+        /*System.out.println(nodeList.size());
         // nodeList = nodeList.reverseByLoop();
         nodeList = nodeList.reverseByRecursive();
         System.out.println(nodeList);
-        System.out.println(nodeList.reverseByRecursive());
+        System.out.println(nodeList.reverseByRecursive());*/
 
-        Node<Integer> head = nodeList.reverse(nodeList.head);
-        System.out.println(head.value);
+        System.out.println("++++++++++++++++++++++++++");
+        nodeList.head = nodeList.reverse2(nodeList.head);
+        System.out.println(nodeList);
     }
 
+    /**
+     * https://www.jianshu.com/p/36ed87e1937a
+     * 定义一个函数，输入一个链表的头结点，反转该链表并输出反转后链表的头结点
+     * 递归实质上就是系统帮你压栈的过程，系统在压栈的时候会保留现场。
+     * 递归法是从最后一个Node开始，在弹栈的过程中将指针顺序置换的
+     * @param head
+     * @return
+     */
     public Node<T> reverse(Node<T> head) {
-       /* Node<T> next = head.next;
-        if (next == null) return head;
-        head = head.next.next;
-        if (head !=null){
-            head.next = null;
+        if (head == null || head.next == null){
+            return head;
         }
-        return reverse(next);*/
-       Node<T> node = head;
-        while (head != null) {
-            Node<T> next = node.next;
-            if (next == null) break;
-            node.next = null;
-            node = next .next;
+        Node<T> temp = head.next;
+        /**
+         * 假设原链表为1->2->3->4,
+         * 递归到head=3,则temp=4，head.next=4, 再次进入递归，满足退出条件，压栈结束，开始弹栈
+         * newHead = 4,
+         * 程序继续执行 temp.next = head就相当于4->3
+         * head.next = null 即把 3结点指向4结点的指针断掉。
+         * 返回新链表的头结点newHead
+         */
+        Node<T> newHead = reverse(head.next);
+        temp.next = head;
+        head.next = null;
+        return newHead ;
+    }
+
+    /**
+     * 迭代遍历
+     * 假设原链表 1->2->3->4
+     *  第一次：head=1,next=2,1.next=null,pre=1    1->null
+     *  第二次：head=2,next=3,2.next=1,pre=2       2->1->null
+     *  第二次：head=3,next=4,3.next=2,pre=3       3->2->1->null
+     *  第四次：head=4,next=null,4.next=3,pre=4    4->3->2->1->null
+     * @param head
+     * @return
+     */
+    public Node<T> reverse2(Node<T> head) {
+        //pre保存先前节点
+        Node<T> pre = null;
+        //next中间临时变量
+        Node<T> next = null;
+        while (head != null){
+            next = head.next;
+            head.next = pre;
+            pre = head;
             head = next;
         }
-        return head;
+        return pre;
     }
 
 }
