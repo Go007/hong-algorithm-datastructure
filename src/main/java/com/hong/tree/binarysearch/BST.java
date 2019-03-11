fbb9ed9268b5ccfcda8c9a71537f3a4a1b580237
macrowang <macrowang@lexin.com>
2019-03-11 10:22:32 +0800

    二分搜索树补充contain()

--- src/main/java/com/hong/tree/binarysearch/BST.java
+++ src/main/java/com/hong/tree/binarysearch/BST.java
@@ -17,11 +17,9 @@ public class BST<K extends Comparable<K>,V> {
         private Node left;
         private Node right;
 
-        public Node(K key, V value, Node left, Node right) {
+        public Node(K key, V value) {
             this.key = key;
             this.value = value;
-            this.left = left;
-            this.right = right;
         }
     }
 
@@ -43,4 +41,56 @@ public class BST<K extends Comparable<K>,V> {
     public boolean isEmpty(){
         return count == 0;
     }
+
+    public void insert(K key,V value){
+        root = insert(root,key,value);
+    }
+
+    public boolean contain(K key){
+        return contain(root,key);
+    }
+
+    /**
+     * 向以node为根节点的二分搜索树中插入新的节点，返回插入后新的根节点
+     * @param node
+     * @param key
+     * @param value
+     * @return
+     */
+    private Node insert(Node node,K key,V value){
+        if (node == null){
+            return new Node(key,value);
+        }
+
+        if (key.compareTo(node.key) == 0){
+            node.value = value;
+        }else if (key.compareTo(node.key) < 0){
+            node.left = insert(node.left,key,value);
+        }else {
+            node.right = insert(node.right,key,value);
+        }
+
+        return node;
+    }
+
+    /**
+     *  查看以node为根节点的二分搜索树中是否存在键值为key的节点
+     * @param node
+     * @param key
+     * @return
+     */
+    public boolean contain(Node node,K key){
+        if (node == null){
+            return false;
+        }
+
+        if (key.compareTo(node.key) == 0){
+            return true;
+        }else if (key.compareTo(node.key) < 0){
+            return contain(node.left,key);
+        }else {
+            return contain(node.right,key);
+        }
+    }
+
 }
