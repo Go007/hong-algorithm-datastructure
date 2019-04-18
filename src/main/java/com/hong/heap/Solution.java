2dbbdaeaad9a5d4461d3955aea5582d916d6a6a2
root <root>
2019-04-18 20:00:06 +0800

    347. 前K个高频元素

--- src/main/java/com/hong/heap/Solution.java
+++ src/main/java/com/hong/heap/Solution.java
@@ -19,10 +19,18 @@ public class Solution {
 
         @Override
         public int compareTo(Freq o) {
-            if (this.freq < o.freq) {
+            /*if (this.freq < o.freq) {
                 return 1;
             } else if (this.freq > o.freq) {
                 return -1;
+            } else {
+                return 0;
+            }*/
+
+            if (this.freq < o.freq) {
+                return -1;
+            } else if (this.freq > o.freq) {
+                return 1;
             } else {
                 return 0;
             }
@@ -46,6 +54,7 @@ public class Solution {
             }
         }
 
+        // 这里因为自定义了比较规则，实际内部是个最小堆
         MaxHeap<Freq> heap = new MaxHeap<>();
         for (Integer key : map.keySet()) {
             if (heap.size() < k) {
@@ -67,39 +76,76 @@ public class Solution {
     public List<Integer> topKFrequent2(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int num : nums) {
-            Integer count = map.get(num);
-            if (count == null) {
-                map.put(num, 1);
-            } else {
-                map.put(num, ++count);
-            }
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        List<Integer> list = new ArrayList<>(map.values());
-        Collections.sort(list, (i, j) -> {
-            if (i.compareTo(j) <= 0) {
-                return 1;
-            } else {
-                return -1;
+        // 桶排序,每个数组里面是一个List
+        List<Integer>[] bucket = new List[nums.length + 1];
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            Integer value = entry.getValue();
+            if (bucket[value] == null) {
+                bucket[value] = new ArrayList<>();
             }
-        });
+            bucket[value].add(entry.getKey());
+        }
 
-        List<Integer> res = new ArrayList<>(k);
-        for (int i = 0; i < k; i++) {
-            for (Integer key : map.keySet()) {
-                if (map.get(key) == list.get(i)) {
-                    res.add(key);
+        List<Integer> res = new LinkedList<>();
+        for (int j = bucket.length - 1; j >= 0; j--) {
+            List<Integer> list = bucket[j];
+            if (list == null) {
+                continue;
+            }
+            for (int n : list) {
+                if (res.size() == k) {
+                    break;
                 }
+                res.add(n);
             }
         }
 
         return res;
     }
 
+    /**
+     * 使用优先队列
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public List<Integer> topKFrequent3(int[] nums, int k) {
+        Map<Integer, Integer> map = new TreeMap<>();
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+
+        // JDK中的PriorityQueue默认是按最小堆来的
+        PriorityQueue<Freq> queue = new PriorityQueue<>();
+        for (Integer key : map.keySet()) {
+            if (queue.size() < k) {
+                queue.add(new Freq(key, map.get(key)));
+            } else if (map.get(key).compareTo(queue.peek().freq) > 0) {
+                queue.poll();
+                queue.add(new Freq(key, map.get(key)));
+            }
+        }
+
+        List<Integer> list = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            list.add(queue.poll().e);
+        }
+
+        return list;
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] nums = {2, 2, 3, 1, 1, 1};
-        List<Integer> list = solution.topKFrequent(nums, 2);
+        int[] nums = {4, 1, -1, 2, -1, 2, 3};
+        List<Integer> list = solution.topKFrequent2(nums, 2);
         System.out.println(list);
     }
 }
