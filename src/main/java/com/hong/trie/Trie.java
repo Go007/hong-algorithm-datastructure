2e65a59ff8f8e79e91193bb0713771b745019717
root <root>
2019-04-21 23:24:12 +0800

    Trie

---
+++ src/main/java/com/hong/trie/Trie.java
@@ -0,0 +1,85 @@
+package com.hong.trie;
+
+import java.util.TreeMap;
+
+/**
+ * @author wanghong
+ * @date 2019/04/21 22:29
+ * <p>
+ * 字典树的基础表示
+ **/
+public class Trie {
+
+    private class Node {
+        // 表示当前节点是否为一个单词的最后一个字符所在的节点（即某个单词的末尾）
+        public boolean isWord;
+        // 当前节点到它所有下一个节点的映射
+        public TreeMap<Character, Node> next;
+
+        public Node(boolean isWord) {
+            this.isWord = isWord;
+            next = new TreeMap<>();
+        }
+
+        public Node() {
+            this(false);
+        }
+    }
+
+    private Node root;
+    private int size;
+
+    public Trie() {
+        root = new Node();
+    }
+
+    /**
+     * 获取Trie中存储的单词数量
+     *
+     * @return
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * 向Trie中添加一个新的单词
+     *
+     * @param word
+     */
+    public void add(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null) {
+                cur.next.put(c, new Node());
+            }
+            cur = cur.next.get(c);
+        }
+
+        if (!cur.isWord) {
+            cur.isWord = true;
+            size++;
+        }
+    }
+
+    /**
+     * 判断word是否在Trie字典树中
+     *
+     * @param word
+     * @return
+     */
+    public boolean contains(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null) {
+                return false;
+            }
+            cur = cur.next.get(c);
+        }
+
+        return cur.isWord;
+    }
+
+}
