06f52c2dfebcd59ae144275286ab320f767e9fdb
wanghong <1536054473@qq.com>
2019-04-22 10:21:15 +0800

    211-添加与搜索单词

---
+++ src/main/java/com/hong/trie/WordDictionary.java
@@ -0,0 +1,94 @@
+package com.hong.trie;
+
+import java.util.TreeMap;
+
+/**
+ * Created by wanghong
+ * Date 2019-04-22 09:26
+ * Description: leetcode - 211.添加与搜索单词-数据结构设计
+ */
+public class WordDictionary {
+
+    private class Node {
+        public boolean isWord;
+        public TreeMap<Character, Node> next;
+
+        public Node(boolean isWord) {
+            this.isWord = isWord;
+            next = new TreeMap<>();
+        }
+
+        public Node() {
+            this(false);
+        }
+
+    }
+
+    private Node root;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public WordDictionary() {
+        root = new Node();
+    }
+
+    /**
+     * Adds a word into the data structure.
+     */
+    public void addWord(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null) {
+                cur.next.put(c, new Node());
+            }
+
+            cur = cur.next.get(c);
+        }
+
+        cur.isWord = true;
+    }
+
+    /**
+     * Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter.
+     */
+    public boolean search(String word) {
+        return match(root, word, 0);
+    }
+
+    private boolean match(Node node, String word, int index) {
+        if (index == word.length()) {
+            return node.isWord;
+        }
+
+        char c = word.charAt(index);
+        if (c != '.') {
+            if (node.next.get(c) == null) {
+                return false;
+            }
+            return match(node.next.get(c), word, index + 1);
+        } else {
+            for (Node value : node.next.values()) {
+                if (match(value, word, index + 1)) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        WordDictionary dictionary = new WordDictionary();
+        dictionary.addWord("bad");
+        dictionary.addWord("dad");
+        dictionary.addWord("mad");
+        System.out.println(dictionary.search("pad"));
+        System.out.println(dictionary.search("bad"));
+        System.out.println(dictionary.search(".ad"));
+        System.out.println(dictionary.search("b.."));
+    }
+
+}
