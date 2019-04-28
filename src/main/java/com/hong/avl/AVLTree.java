f97b501703bc7205a4a3cecff0317df6d250409a
root <root>
2019-04-28 14:54:38 +0800

    AVL树的删除操作

--- src/main/java/com/hong/avl/AVLTree.java
+++ src/main/java/com/hong/avl/AVLTree.java
@@ -311,18 +311,6 @@ public class AVLTree<K extends Comparable<K>, V> {
         return minimum(node.left);
     }
 
-    private Node removeMin(Node node) {
-        if (node.left == null) {
-            Node rightNode = node.right;
-            node.right = null;
-            size--;
-            return rightNode;
-        }
-
-        node.left = removeMin(node.left);
-        return node;
-    }
-
     // 从二分搜索树中删除键为key的节点
     public V remove(K key) {
 
@@ -339,42 +327,69 @@ public class AVLTree<K extends Comparable<K>, V> {
         if (node == null)
             return null;
 
+        Node retNode;
         if (key.compareTo(node.key) < 0) {
             node.left = remove(node.left, key);
-            return node;
+            retNode = node;
         } else if (key.compareTo(node.key) > 0) {
             node.right = remove(node.right, key);
-            return node;
-        } else {   // key.compareTo(node.key) == 0
-
-            // 待删除节点左子树为空的情况
+            retNode = node;
+        } else {
             if (node.left == null) {
                 Node rightNode = node.right;
                 node.right = null;
                 size--;
-                return rightNode;
-            }
-
-            // 待删除节点右子树为空的情况
-            if (node.right == null) {
+                retNode = rightNode;
+            } else if (node.right == null) {
                 Node leftNode = node.left;
                 node.left = null;
                 size--;
-                return leftNode;
+                retNode = leftNode;
+            } else {
+                Node successor = minimum(node.right);
+                successor.right = remove(node.right, successor.key);
+                successor.left = node.left;
+
+                node.left = node.right = null;
+
+                retNode = successor;
             }
-
-            // 待删除节点左右子树均不为空的情况
-
-            // 找到比待删除节点大的最小节点, 即待删除节点右子树的最小节点
-            // 用这个节点顶替待删除节点的位置
-            Node successor = minimum(node.right);
-            successor.right = removeMin(node.right);
-            successor.left = node.left;
-
-            node.left = node.right = null;
-
-            return successor;
         }
+
+        if (retNode == null) {
+            return null;
+        }
+
+        // 更新height
+        retNode.height = Math.max(getHeight(retNode.left), getHeight(retNode.right)) + 1;
+
+        // 计算平衡因子
+        int balanceFactor = getBalanceFactor(retNode);
+
+        // 平衡维护
+        // LL
+        if (balanceFactor > 1 && getBalanceFactor(retNode.left) >= 0) {
+            return rightRotate(retNode);
+        }
+
+        // RR
+        if (balanceFactor < -1 && getBalanceFactor(retNode.right) <= 0) {
+            return leftRotate(retNode);
+        }
+
+        // LR
+        if (balanceFactor > 1 && getBalanceFactor(retNode.left) < 0) {
+            retNode.left = leftRotate(retNode.left);
+            return rightRotate(retNode);
+        }
+
+        // RL
+        if (balanceFactor < -1 && getBalanceFactor(retNode.right) > 0) {
+            retNode.right = rightRotate(retNode.right);
+            return leftRotate(retNode);
+        }
+
+        return retNode;
     }
 
     public static void main(String[] args) {
@@ -399,6 +414,12 @@ public class AVLTree<K extends Comparable<K>, V> {
 
             System.out.println("is BST : " + map.isBST());
             System.out.println("is Balanced : " + map.isBalanced());
+
+            for (String word : words) {
+                map.remove(word);
+                if (!map.isBST() || !map.isBalanced())
+                    throw new RuntimeException();
+            }
         }
 
         System.out.println();
