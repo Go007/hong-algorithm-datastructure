62c7b2ce6aa29f629b38cb00c5654aaabd966d7b
root <root>
2019-05-03 22:31:02 +0800

    Java中的hashCode()

---
+++ src/main/java/com/hong/leetcode/Solutino387.java
@@ -0,0 +1,24 @@
+package com.hong.leetcode;
+
+/**
+ * @author wanghong
+ * @date 2019/05/03 16:54
+ * 387. 字符串中的第一个唯一字符
+ **/
+public class Solutino387 {
+
+    public int firstUniqChar(String s) {
+        int[] freq = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s.charAt(i) - 'a'] == 1){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+}
