52f3950333c2a96c1f883eb2d2ecb248bc7f03e0
root <root>
2019-05-04 22:59:51 +0800

    3. 无重复字符的最长子串

--- src/main/java/com/hong/leetcode/Solution3.java
+++ src/main/java/com/hong/leetcode/Solution3.java
@@ -70,9 +70,45 @@ public class Solution3 {
         return maxLength;
     }
 
+    /**
+     * 建立一个256位大小的整型数组 freg ，用来建立字符和其出现次数之间的映射。
+     * <p>
+     * 维护一个滑动窗口，窗口内的都是没有重复的字符，去尽可能的扩大窗口的大小，窗口不停的向右滑动。
+     * <p>
+     * 如果当前遍历到的字符从未出现过，那么直接扩大右边界；
+     * 如果当前遍历到的字符出现过，则缩小窗口（左边索引向右移动），然后继续观察当前遍历到的字符；
+     * 重复（1）（2），直到左边索引无法再移动；
+     * 维护一个结果res，每次用出现过的窗口大小来更新结果 res，最后返回 res 获取结果。
+     * <p>
+     * 时间复杂度: O(len(s))
+     * 空间复杂度: O(len(charset))
+     *
+     * @param s
+     * @return
+     */
+    public static int lengthOfLongestSubstring3(String s) {
+        int[] freq = new int[256];
+        int l = 0, r = -1;
+        int res = 0;
+        char[] chars = s.toCharArray();
+        int size = chars.length;
+        while (l < size) {
+            if (r + 1 < size && freq[chars[r + 1]] == 0) {
+                r++;
+                freq[chars[r]]++;
+            } else {
+                freq[chars[l]]--;
+                l++;
+            }
+            res = Math.max(res, r-l+1);
+        }
+
+        return res;
+    }
+
     public static void main(String[] args) {
         String s = "bdvdf";
-        System.out.println(lengthOfLongestSubstring2(s));
+        System.out.println(lengthOfLongestSubstring3(s));
     }
 
 }
