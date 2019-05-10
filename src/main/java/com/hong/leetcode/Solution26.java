55534cc68664f37624a8ed8f24e064e32e6f6072
wanghong <wanghong@chinacsci.com>
2019-05-10 17:09:29 +0800

    26. 删除排序数组中的重复项

--- src/main/java/com/hong/leetcode/Solution26.java
+++ src/main/java/com/hong/leetcode/Solution26.java
@@ -8,6 +8,32 @@ package com.hong.leetcode;
  */
 public class Solution26 {
 
+    public static int removeDuplicates2(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int l = 0;
+        int r = nums.length - 1;
+        int len = nums.length;
+        while (l < r) {
+            if (nums[l] == nums[r]) {
+                int i = l + 1;
+                int j = r + 1;
+                while (i <= r && j < len) {
+                    nums[i] = nums[j];
+                    i++;
+                    j++;
+                }
+                len = len - r + l;
+            } else {
+
+            }
+        }
+
+        return len;
+    }
+
     public static int removeDuplicates(int[] nums) {
         if (nums == null || nums.length == 0) {
             return 0;
@@ -30,11 +56,43 @@ public class Solution26 {
         return len;
     }
 
+    public static int removeDuplicates3(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int len = nums.length;
+        int i = len - 1;
+
+        while (i > 0) {
+            if (nums[i] != nums[i - 1]) {
+                i--;
+                continue;
+            }
+
+            int r = i;
+            while (i > 0 && nums[i] == nums[i - 1]) {
+                i--;
+            }
+
+            int j = i;
+            for (int k = r + 1; k < len; k++) {
+                nums[++j] = nums[k];
+            }
+
+            len = len - (r - i);
+            i--;
+        }
+
+        return len;
+    }
+
     public static void main(String[] args) {
         int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
-        System.out.println(removeDuplicates(nums));
-        for (int i : nums) {
-            System.out.print(i + " ");
+        int len = removeDuplicates3(nums);
+        System.out.println(len);
+        for (int i = 0; i < len; i++) {
+            System.out.print(nums[i] + " ");
         }
     }
 
