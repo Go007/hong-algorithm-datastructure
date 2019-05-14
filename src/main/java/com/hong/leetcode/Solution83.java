d78342fa31065a3b10cf81357ca392e4a0fe696b
root <root>
2019-05-14 22:24:17 +0800

    83. 删除排序链表中的重复元素

--- src/main/java/com/hong/leetcode/Solution83.java
+++ src/main/java/com/hong/leetcode/Solution83.java
@@ -27,10 +27,28 @@ public class Solution83 {
         return head;
     }
 
+    public static ListNode deleteDuplicates2(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        if (head.val == head.next.val) {
+            ListNode pre = head;
+            while (pre.next != null && pre.val == pre.next.val) {
+                pre = pre.next;
+            }
+            head.next = pre.next;
+            pre.next = null;
+        }
+
+        head.next = deleteDuplicates2(head.next);
+        return head;
+    }
+
     public static void main(String[] args) {
         int[] nums = {1, 2, 2, 3, 3, 3};
         ListNode head = new ListNode(nums);
-        head = deleteDuplicates(head);
+        head = deleteDuplicates2(head);
         System.out.println(head);
     }
 
