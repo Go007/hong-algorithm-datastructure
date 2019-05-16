ab82f0b1b6a88118ca03e50813f5d4727544410f
root <root>
2019-05-16 23:23:31 +0800

    61. 旋转链表

--- src/main/java/com/hong/leetcode/Solution61.java
+++ src/main/java/com/hong/leetcode/Solution61.java
@@ -10,50 +10,64 @@ import com.hong.linked.ListNode;
  */
 public class Solution61 {
 
+    /**
+     * @param head
+     * @param k
+     * @return
+     */
     public static ListNode rotateRight(ListNode head, int k) {
         if (head == null || head.next == null || k <= 0) {
             return head;
         }
 
-        ListNode prev = head;
-        ListNode oldTail = null;
+        /**
+         * 找到倒数第 k+1 个节点，
+         *  k = k > 链表长度 ？ k % 链表长度 : k;
+         * 遍历到尾节点后，回到头节点继续遍历，直到到达 k+1 个计数，
+         * 说是循环旋转，但其实本质上是将尾部向前数第K个元素作为头，原来的头接到原来的尾上
+         */
+        ListNode tail = head;
+        int size = 1;
+        while (tail.next != null) {
+            size++;
+            tail = tail.next;
+        }
+        k = k > size ? k % size : k;
 
-        for (int i = 1; i < k; i++) {
-            if (prev.next == null) {
-                oldTail = prev;
-                prev = head;
-            } else {
-                prev = prev.next;
-            }
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+        ListNode p = dummyHead, prev = dummyHead;
+        for (int i = 1; i <= k; i++) {
+            p = p.next;
         }
 
-        ListNode newHead = null;
-        ListNode tail = prev.next;
-        if (tail == null) {
-            if (prev != head) {
-
-            } else {
-                newHead = head;
-            }
+        while (p.next != null) {
+            p = p.next;
+            prev = prev.next;
         }
 
+        dummyHead.next = null;
+        ListNode newHead = prev.next;
+        if (newHead == null) {
+            return head;
+        }
 
-        if (tail.next == null) {
-            prev.next = null;
-            tail.next = head;
+        if (newHead.next == null) {
+            newHead.next = head;
         } else {
-
-        }
-
-
-        if (newHead != null) {
-            tail.next = null;
-            oldTail.next = head;
-        } else {
             tail.next = head;
         }
+        prev.next = null;
 
         return newHead;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2};
+        ListNode head = new ListNode(nums);
+        System.out.println(head);
+        head = rotateRight(head, 2);
+        System.out.println(head);
     }
 
 }
