5dec5314d60f13cd31193c0ccab835e5c07097ea
wanghong <wanghong@chinacsci.com>
2019-07-18 17:22:34 +0800

    LFU

--- src/main/java/com/hong/lfu/LFUCache2.java
+++ src/main/java/com/hong/lfu/LFUCache2.java
@@ -19,7 +19,7 @@ public class LFUCache2<K, V> {
     /**
      * 将频次看成优先级， 频次 --》 拥有该频次的Node链表，三个元素：链表头节点，链表尾节点，链表长度
      */
-    private Map<Integer,FreqAggregation> freqMap;
+    private Map<Integer, FreqAggregation> freqMap;
 
     /**
      * 当前最小频次
@@ -38,17 +38,27 @@ public class LFUCache2<K, V> {
     }
 
     public void put(K key, V value) {
-        if (capacity <= 0){
+        if (capacity <= 0) {
             return;
         }
 
         Node node = keyMap.get(key);
-        if (node != null){
+        FreqAggregation fa = freqMap.get(node.freq);
+
+        if (node != null) {
             node.value = value;
-            int freq = node.freq;
-            FreqAggregation fa = freqMap.get(freq);
+
+        }else {
+            node = new Node(key, value);
+            if (fa == null){
+                fa = new FreqAggregation(node);
+            }else{
+
+            }
 
         }
+
+
     }
 
     /**
@@ -66,34 +76,38 @@ public class LFUCache2<K, V> {
          * 频次          Node聚集
          * 1           [n0]   FreqAggregation1
          * 2           [n2,n3]   FreqAggregation2
-         *
+         * <p>
          * 则FreqAggregation1.next = FreqAggregation2
          * minFreq = 1
-         *
+         * <p>
          * put()操作，插入n4，判断key是否已经存在：
          * 1.存在更新value,
-         *   由n4.freq 找到 fa4,将n4从fa4中删除：
-         *   如果删除后 size == 0，fa4.prev.next = fa4.next; fa4.next.prev = fa4.prev;
-         *   从map中删除fa4，minFreq = fa4.prev.head.freq;
-         *   n4.freq++ ,找fa5:
-         *   找到了，则将 fa5.head = n4；
-         *   没有找到，则新建fa5，放入map中；
-         *   if minFreq > n4.freq,则 minFreq ==  n4.freq; fa5.next = minFreq对应的fa0；
-         *
-         *
+         * 由n4.freq 找到 fa4,将n4从fa4中删除：
+         * 如果删除后 size == 0，fa4.prev.next = fa4.next; fa4.next.prev = fa4.prev;
+         * 从map中删除fa4，minFreq = fa4.prev.head.freq;
+         * n4.freq++ ,找fa5:
+         * 找到了，则将 fa5.head = n4；
+         * 没有找到，则新建fa5，放入map中；
+         * if minFreq > n4.freq,则 minFreq ==  n4.freq; fa5.next = minFreq对应的fa0；
+         * <p>
+         * <p>
          * 2.不存在,发现 keyMap.size == capacity,触发执行淘汰策略。
-         *   根据 minFreq = 1，找到 FreqAggregation1，删除头节点，size--变为0；
-         *   插入n4，
-         *
-         *   再由next = FreqAggregation2，将minFreq更新为 FreqAggregation2.head.freq,
-         *   同时删除 FreqAggregation1。
-         *
+         * 根据 minFreq = 1，找到 FreqAggregation1，删除头节点，size--变为0；
+         * 插入n4，
+         * <p>
+         * 再由next = FreqAggregation2，将minFreq更新为 FreqAggregation2.head.freq,
+         * 同时删除 FreqAggregation1。
          */
         FreqAggregation prev;
         FreqAggregation next;
+
+        public FreqAggregation(Node node){
+            head = tail = node;
+            size++;
+        }
     }
 
-    private static class Node<K,V> {
+    private static class Node<K, V> {
         K key;
         V value;
         int freq; // 访问频次
