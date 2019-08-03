6174055bbde9bd53e503e4b03789c6cfeb6c0c58
root <root>
2019-08-03 17:04:10 +0800

    119. 杨辉三角 II

--- src/main/java/com/hong/leetcode/Solution119.java
+++ src/main/java/com/hong/leetcode/Solution119.java
@@ -17,8 +17,8 @@ public class Solution119 {
      * @param rowIndex
      * @return
      */
-    public List<Integer> getRow(int rowIndex) {
-        List<Integer> result = new ArrayList<>(rowIndex+1);
+    public static List<Integer> getRow(int rowIndex) {
+        List<Integer> result = new ArrayList<>(rowIndex + 1);
 
         if (rowIndex == 0) {
             result.add(1);
@@ -26,16 +26,41 @@ public class Solution119 {
             result.add(1);
             result.add(1);
         } else {
-            result.set(0,1);
+            result.add(1);
+            result.add(1);
             for (int i = 1; i < rowIndex; i++) {
-                int a = result.get(i-1);
-                int b = result.get(i);
-                result.set(i,a+b);
+                result.add(1);
+                for (int j = 0; j < i; j++) {
+                    result.add(result.get(0) + result.get(1));
+                    result.remove(0);
+                }
+                result.add(1);
+                result.remove(0);
             }
-            result.set(rowIndex,1);
         }
 
         return result;
     }
 
+    /**
+     * 获取杨辉三角的指定行
+     * 直接使用组合公式C(n,i) = n!/(i!*(n-i)!)
+     * 则第(i+1)项是第i项的倍数=(n-i)/(i+1);
+     */
+    public static List<Integer> getRow2(int rowIndex) {
+        List<Integer> res = new ArrayList<>(rowIndex + 1);
+        long cur = 1;
+        for (int i = 0; i <= rowIndex; i++) {
+            res.add((int) cur);
+            cur = cur * (rowIndex-i)/(i+1);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> result = getRow(3);
+        for (int i : result) {
+            System.out.print(i + ",");
+        }
+    }
 }
