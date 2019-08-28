0083a875eef6279e9887a32b58b1a21a815351c7
wanghong <wanghong@chinacsci.com>
2019-08-28 17:48:08 +0800

    114. 二叉树展开为链表

---
+++ src/main/java/com/hong/leetcode/Solution114.java
@@ -0,0 +1,15 @@
+package com.hong.leetcode;
+
+/**
+ * Created by wanghong
+ * Date 2019-08-28 15:46
+ * Description:
+ * 114. 二叉树展开为链表
+ */
+public class Solution114 {
+
+    public void flatten(TreeNode root) {
+        // TODO
+    }
+
+}
