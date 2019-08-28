cd6408d1dbe293b39b998a3efec1d224d3805da6
root <root>
2019-08-28 21:27:27 +0800

    114. 二叉树展开为链表

--- src/main/java/com/hong/leetcode/Solution114.java
+++ src/main/java/com/hong/leetcode/Solution114.java
@@ -8,8 +8,37 @@ package com.hong.leetcode;
  */
 public class Solution114 {
 
+    TreeNode tail = null;
     public void flatten(TreeNode root) {
-        // TODO
+        if (root == null){
+            return;
+        }
+        if (tail != null){
+            // 这里注意要将左子树置空
+            tail.left = null;
+            tail.right = root;
+        }
+
+        tail = root;
+        // 前序：注意备份右子节点，规避下一节点篡改
+        TreeNode copyRight = root.right;
+        flatten(root.left);
+        flatten(copyRight);
+    }
+
+    /**
+     * 递归既简单又复杂
+     */
+    TreeNode pre = null;
+    public void flatten2(TreeNode root) {
+        if (root == null){
+            return;
+        }
+        flatten2(root.right);
+        flatten2(root.left);
+        root .right = pre;
+        root.left = null;
+        pre = root;
     }
 
 }
