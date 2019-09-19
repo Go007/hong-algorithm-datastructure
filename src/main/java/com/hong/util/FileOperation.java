95297901edc50cccb14d748231003aec877cf736
wanghong <wanghong@chinacsci.com>
2019-09-19 15:27:43 +0800

    Java8 Lambda test

--- src/main/java/com/hong/util/FileOperation.java
+++ src/main/java/com/hong/util/FileOperation.java
@@ -4,9 +4,8 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Locale;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @author wanghong
@@ -83,6 +82,61 @@ public class FileOperation {
         System.out.println(localMaven);
         File file = new File(localMaven);
         System.out.println(file.getAbsoluteFile());
+
+        List<Map<String,Object>> l0 = new ArrayList<>();
+        Map<String,Object> m0 = new HashMap<>();
+        m0.put("proCode",26);
+        m0.put("proId",1);
+        Map<String,Object> m1 = new HashMap<>();
+        m1.put("proCode",28);
+        m1.put("proId",2);
+        Map<String,Object> m6 = new HashMap<>();
+        m6.put("proCode",29);
+        m6.put("proId",1);
+        l0.add(m0);
+        l0.add(m1);
+        l0.add(m6);
+
+        List<Object> collect = l0.stream().filter(m -> (Integer) m.get("proCode") == 26).map(m -> m.get("proId")).collect(Collectors.toList());
+        collect.forEach(p -> System.out.println(p));
+
+        List<Map<String, Object>> ll = l0.stream().filter(m -> !collect.contains(m.get("proId"))).collect(Collectors.toList());
+        ll.forEach(m -> System.out.println(m));
+
+
+        System.out.println("==========");
+       // Map<Object, List<Map<String, Object>>> proId = l0.stream().filter(m -> (Integer)m.get("proCode")!=26).collect(Collectors.groupingBy(m -> m.get("proId")));
+        Map<Object, List<Object>> proId = l0.stream().collect(Collectors.groupingBy(m -> m.get("proId"), Collectors.mapping(m -> m.get("proCode"), Collectors.toList())));
+
+
+        for (Map.Entry<Object, List<Object>> entry:proId.entrySet()){
+            System.out.println(entry.getKey() + ":" + entry.getValue());
+        }
+
+        System.out.println("====================");
+        List<Map<String,Object>> l1 = new ArrayList<>();
+        Map<String,Object> m2 = new HashMap<>();
+        m2.put("proCode",26);
+        l1.add(m2);
+
+        List<Object> l2 = l0.stream().map(m -> m.get("proCode")).collect(Collectors.toList());
+        List<Object> l3 = l1.stream().map(m -> m.get("proCode")).collect(Collectors.toList());
+        List<Object> l4 = l2.stream().filter(c -> !l3.contains(c)).collect(Collectors.toList());
+        l4.forEach(c -> System.out.println(c));
+
+        List<Map<String, Object>> l5 = l0.stream().filter(m -> l4.contains(m.get("proCode"))).collect(Collectors.toList());
+        l5.forEach(d -> System.out.println(d));
+
+
+        System.out.println("++++++++++++");
+        List<Integer> num = new ArrayList<>();
+        num.add(1);
+        num.add(2);
+        num.add(3);
+        num.add(4);
+        num.add(5);
+        List<Integer> subList = num.subList(1, 2);
+        System.out.println(subList);
     }
 }
 
