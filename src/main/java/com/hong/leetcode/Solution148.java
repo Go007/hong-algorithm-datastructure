7d44286e62b410f37620d82d4884eadb2306e401
hong <1536054473@qq.com>
2019-11-23 18:07:18 +0800

    148. 排序链表 快排

--- src/main/java/com/hong/leetcode/Solution148.java
+++ src/main/java/com/hong/leetcode/Solution148.java
@@ -78,6 +78,8 @@ public class Solution148 {
         return dummyHead.next;
     }
 
+    //====================================================================================//
+
     /****************************************************/
     /** TODO 链表排序的自底向上的归并排序实现
      *要求空间复杂度是 O(1)，因此不能使用递归。因此这里使用 bottom-to-up 的算法来解决
@@ -111,8 +113,8 @@ public class Solution148 {
         dummyHead.next = head;
         //共循环logN次，每次循环都将进行一次全链表的遍历并逐步恢复次序
         for (int size = 1;size < length;size += size){
-            //将链表分为四段，第一段为已排序的部分,第二段为待排序的第一部分(由left表示头节点),
-            // 第三段为待排序的第二部分(由right表示头节点),第四段为未排序的部分(由tail表示头节点)
+            //将链表分为四段，第一段为已排序的部分(由dummyHead.next表示头节点),第二段为待排序的第一部分(由left表示头节点),
+            // 第三段为待排序的第二部分(由right表示头节点),第四段为未排序的部分(由cur表示头节点)
             ListNode cur = dummyHead.next;
             ListNode tail = dummyHead;
             ListNode left = null;
@@ -150,12 +152,55 @@ public class Solution148 {
         return next;
     }
 
+    //====================================================================================//
+
+    /**
+     * TODO 快排版本
+     * 交换结点版本，非伪排序只交换数值
+     * @param head
+     * @return
+     */
+    public ListNode sortList3(ListNode head) {
+        if(head==null||head.next==null) return head;
+        // 没有条件，创造条件。自己添加头节点，最后返回时去掉即可。
+        ListNode newHead=new ListNode(-1);
+        newHead.next=head;
+        return quickSort(newHead,null);
+    }
+
+    // 带头结点的链表快速排序
+    private ListNode quickSort(ListNode head,ListNode end){
+        if (head==end||head.next==end||head.next.next==end) return head;
+        // 将小于划分点的值存储在临时链表中
+        ListNode tmpHead=new ListNode(-1);
+        // partition为划分点，p为链表指针，tp为临时链表指针
+        ListNode partition=head.next,p=partition,tp=tmpHead;
+        // 将小于划分点的结点放到临时链表中
+        while (p.next!=end){
+            if (p.next.val<partition.val){
+                tp.next=p.next;
+                tp=tp.next;
+                p.next=p.next.next;
+            }else {
+                p=p.next;
+            }
+        }
+        // 合并临时链表和原链表，将原链表接到临时链表后面即可
+        tp.next=head.next;
+        // 将临时链表插回原链表，注意是插回！（不做这一步在对右半部分处理时就断链了）
+        head.next=tmpHead.next;
+        quickSort(head,partition);
+        quickSort(partition,end);
+        // 题目要求不带头节点，返回结果时去除
+        return head.next;
+    }
+
     public static void main(String[] args) {
         Solution148 s = new Solution148();
-        int[] nums = {4,2,1,3};
+        int[] nums = {4,3,1,2,5};
         ListNode head = new ListNode(nums);
         System.out.println(head);
-        head = s.sortList2(head);
+        head = s.sortList3(head);
         System.out.println(head);
        // System.out.println(s.cut(head,3));
        // System.out.println(head);
