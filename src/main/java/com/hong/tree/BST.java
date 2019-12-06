0da6bbce4162d14e8efbd134a0e9e757f439792f
wanghong <wanghong@chinacsci.com>
2019-12-06 14:29:22 +0800

    二分搜索树的前驱和后继

--- src/main/java/com/hong/tree/BST.java
+++ src/main/java/com/hong/tree/BST.java
@@ -71,7 +71,8 @@ public class BST<K extends Comparable<K>, V> {
      * @return
      */
     public V search(K key) {
-        return search(root, key);
+        Node node = search(root, key);
+        return node == null ? null:node.value;
     }
 
     // 二分搜索树的前序遍历
@@ -216,12 +217,12 @@ public class BST<K extends Comparable<K>, V> {
      * @param key
      * @return
      */
-    public V search(Node node, K key) {
+    public Node search(Node node, K key) {
         if (node == null) {
             return null;
         }
         if (key.compareTo(node.key) == 0) {
-            return node.value;
+            return node;
         } else if (key.compareTo(node.key) < 0) {
             return search(node.left, key);
         } else {
@@ -581,6 +582,99 @@ public class BST<K extends Comparable<K>, V> {
         return node;
     }
 
+    /**
+     * 查找key的前驱
+     * 如果不存在key的前驱(key不存在, 或者key是整棵二叉树中的最小值), 则返回NULL
+     * @param key
+     * @return
+     */
+    public K predecessor(K key){
+        Node node = search(root,key);
+        if (node == null) return null;
+
+        // 如果key的左子树不为空，则其左子树的最大值即为key的前驱
+        if (node.left != null){
+            return maximum(node.left).key;
+        }
+
+        // 否则，key的前驱存在于从根结点到key的路径上，在这个路径上找到比key小的最大值，即为key的前驱
+        Node preNode = predecessorFromAncestor(root,key);
+        return preNode == null ? null:preNode.key;
+    }
+
+    /**
+     * 在以node为根的二叉搜索树中，寻找key的祖先中比key小的最大值所在节点，递归算法
+     * 算法调用前已保证key存在于以node为根的二叉树中
+     * @param node
+     * @param key
+     * @return
+     */
+    private Node predecessorFromAncestor(Node node, K key) {
+        if(node.key.compareTo(key) == 0)
+            return null;
+
+        if(key.compareTo(node.key) < 0)
+            // 如果当前节点大于key, 则当前节点不可能是比key小的最大值
+            // 向下搜索到的结果直接返回
+            return predecessorFromAncestor(node.left, key);
+        else{
+            assert key.compareTo(node.key) > 0;
+            // 如果当前节点小于key, 则当前节点有可能是比key小的最大值
+            // 向右继续搜索, 将结果存储到tempNode中
+            Node tempNode = predecessorFromAncestor(node.right, key);
+            if(tempNode != null)
+                return tempNode;
+            else
+                // 如果tempNode为空, 则当前节点即为结果
+                return node;
+        }
+    }
+
+    // 查找key的后继, 递归算法
+    // 如果不存在key的后继(key不存在, 或者key是整棵二叉树中的最大值), 则返回NULL
+    public K successor(K key){
+        Node node = search(root, key);
+        // 如果key所在的节点不存在, 则key没有前驱, 返回NULL
+        if(node == null)
+            return null;
+
+        // 如果key所在的节点右子树不为空,则其右子树的最小值为key的后继
+        if(node.right != null)
+            return minimum(node.right).key;
+
+        // 否则, key的后继在从根节点到key的路径上, 在这个路径上寻找到比key大的最小值, 即为key的后继
+        Node sucNode = successorFromAncestor(root, key);
+        return sucNode == null ? null : sucNode.key;
+    }
+
+    /**
+     * 在以node为根的二叉搜索树中, 寻找key的祖先中,比key大的最小值所在节点, 递归算法
+     * 算法调用前已保证key存在于以node为根的二叉树中
+     * @param node
+     * @param key
+     * @return
+     */
+    private Node successorFromAncestor(Node node, K key) {
+        if(node.key.compareTo(key) == 0)
+            return null;
+
+        if(key.compareTo(node.key) > 0)
+            // 如果当前节点小于key, 则当前节点不可能是比key大的最小值
+            // 向下搜索到的结果直接返回
+            return successorFromAncestor(node.right, key);
+        else{
+            assert(key.compareTo(node.key) < 0);
+            // 如果当前节点大于key, 则当前节点有可能是比key大的最小值
+            // 向左继续搜索, 将结果存储到tempNode中
+            Node tempNode = successorFromAncestor(node.left, key);
+            if(tempNode != null)
+                return tempNode;
+            else
+                // 如果tempNode为空, 则当前节点即为结果
+                return node;
+        }
+    }
+
     // 测试二分搜索树
     public static void main(String[] args) {
         BST<Integer, String> bst = new BST<>();
@@ -589,6 +683,9 @@ public class BST<K extends Comparable<K>, V> {
             bst.insert(k, k + "");
         }
 
+        System.out.println(bst.floor(26));
+        System.out.println(bst.ceil(26));
+        System.out.println("==========");
         bst.levelOrder();
         // bst.remove(16);
         System.out.println();
