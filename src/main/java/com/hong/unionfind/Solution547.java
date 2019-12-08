1a1f9bc59e749d68e7febd1d3eed93d5a6c86979
hong <1536054473@qq.com>
2019-12-08 18:26:34 +0800

    Leetcode-547. 朋友圈 并查集的应用

--- src/main/java/com/hong/unionfind/Solution547.java
+++ src/main/java/com/hong/unionfind/Solution547.java
@@ -8,7 +8,7 @@ import java.util.TreeSet;
  * @date 2019/04/24 20:58
  * Leetcode-547. 朋友圈
  **/
-public class Solution {
+public class Solution547 {
     public int findCircleNum(int[][] M) {
         int n = M.length;
         UnionFind6 un = new UnionFind6(n);
@@ -30,7 +30,7 @@ public class Solution {
 
     public static void main(String[] args) {
         int[][] M = {{1,1,0},{1,1,0},{0,0,1}};
-        Solution solution = new Solution();
+        Solution547 solution = new Solution547();
         System.out.println(solution.findCircleNum(M));
     }
 }
