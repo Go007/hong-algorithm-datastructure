328f308c4e2ca4568b49a2ed84769947a508bc9e
hong <1536054473@qq.com>
2020-06-11 23:17:43 +0800

    哈希表

--- src/main/java/com/hong/test/TestList.java
+++ src/main/java/com/hong/test/TestList.java
@@ -1,6 +1,7 @@
 package com.hong.test;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -8,7 +9,7 @@ import java.util.List;
  * @Author wanghong
  * @Date 2020/6/11 16:44
  * @Version V1.0
- *
+ * <p>
  * List 如何一边遍历，一边删除？
  **/
 public class TestList {
@@ -22,9 +23,20 @@ public class TestList {
         System.out.println(list);
     }
 
-    public static List<String> wrongDelete(List<String> list){
-        for (String s:list){
-            if ("a".equals(s)){
+    public static List<String> wrongDelete(List<String> list) {
+        for (String s : list) {
+            if ("b".equals(s)) {
+                list.remove(s);
+            }
+        }
+        return list;
+    }
+
+    public static List<String> wrongDelete2(List<String> list) {
+        Iterator<String> it = list.iterator();
+        while (it.hasNext()) {
+            String s = it.next();
+            if ("a".equals(s)) {
                 list.remove(s);
             }
         }
