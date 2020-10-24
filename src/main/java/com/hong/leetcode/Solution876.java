01565088229d5634c34c57fefd46fba8d920c787
hong <1536054473@qq.com>
2020-10-24 18:01:59 +0800

    876. 链表的中间结点 update

--- src/main/java/com/hong/leetcode/Solution876.java
+++ src/main/java/com/hong/leetcode/Solution876.java
@@ -45,6 +45,27 @@ public class Solution876 {
     }
 
     /**
+     * 如果链表长度为偶数，返回中间第一个
+     * 如 1-> 2 -> 3 -> 4 ，返回 2
+     *
+     * @param head
+     * @return
+     */
+    public ListNode middleNode3(ListNode head) {
+        ListNode pre = null;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // 通过判断 fast == null (偶数) 或 fast.next == null 推断出 链表长度是奇数还是偶数
+        return fast == null ? pre : slow;
+    }
+
+    /**
      * 上述的两个方法孰优孰劣呢？
      * 网上很多说什么方法一过了两遍链表，方法二只过了一遍。
      * 但其实，方法二用了两个指针来遍历，所以两个方法过的遍数都是一样的。
@@ -53,8 +74,8 @@ public class Solution876 {
      */
     public static void main(String[] args) {
         Solution876 s = new Solution876();
-        int[] arr = {1, 2, 3, 4, 5, 6};
+        int[] arr = {1, 2, 3, 4};
         ListNode head = new ListNode(arr);
-        System.out.println(s.middleNode2(head));
+        System.out.println(s.middleNode3(head));
     }
 }
