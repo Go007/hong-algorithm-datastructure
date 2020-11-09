a194ad3520b56ad9a5501605eb13c2de2a21adec
wanghong <wanghong@chinacsci.com>
2020-11-09 18:45:26 +0800

    27. 移除元素

---
+++ src/main/java/com/hong/leetcode/Solution27.java
@@ -0,0 +1,17 @@
+package com.hong.leetcode;
+
+/**
+ * @author ：wanghong
+ * @date ：2020-11-09 18:16
+ * @description：27. 移除元素
+ */
+public class Solution27 {
+
+    public int removeElement(int[] nums, int val) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+    }
+
+}
