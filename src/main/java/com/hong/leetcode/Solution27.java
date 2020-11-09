c2c27b314fc2f0fa7ae82ff2f3474b61ba9c0ee3
wanghong <1536054473@qq.com>
2020-11-09 22:15:02 +0800

    27. 移除元素

--- src/main/java/com/hong/leetcode/Solution27.java
+++ src/main/java/com/hong/leetcode/Solution27.java
@@ -12,6 +12,37 @@ public class Solution27 {
             return 0;
         }
 
+        int k = nums.length - 1;
+        int i = 0;
+        for (; i <= k; i++) {
+            if (nums[i] != val) {
+                continue;
+            }
+
+            while (k > i && nums[k] == val) {
+                k--;
+            }
+
+            if (k <= i) {
+                break;
+            }
+
+            nums[i] = nums[k];
+            k--;
+        }
+
+        return i;
+    }
+
+    public static void main(String[] args) {
+        Solution27 s = new Solution27();
+        int[] nums = {3,3,2,7,2,3,5};
+        int len = s.removeElement(nums, 3);
+        System.out.println(len);
+        for (int i = 0; i < len; i++) {
+            System.out.print(nums[i] + ",");
+        }
+        System.out.println();
     }
 
 }
