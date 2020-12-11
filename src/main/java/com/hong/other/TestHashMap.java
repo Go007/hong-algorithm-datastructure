2c9092536cf87afc3faa0f43970b6634ed1c7704
wanghong <wanghong@chinacsci.com>
2020-12-11 14:51:07 +0800

    HashMap之hashCode()测试

--- src/main/java/com/hong/other/TestHashMap.java
+++ src/main/java/com/hong/other/TestHashMap.java
@@ -1,6 +1,7 @@
 package com.hong.other;
 
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author ：wanghong
@@ -26,6 +27,25 @@ public class TestHashMap {
             map2.put(j,j);
         }
         System.out.println(map2.size());
+
+        /////////////////////////////////
+        Map<Person,String> m = new HashMap<>();
+        Person p = new Person();
+        p.setName("A");
+        m.put(p,"X");
+        p.setName("B");
+        m.put(p,"Y");
+        /**
+         * 因为Person类中重写了hashCode(),依赖String类的hashCode()
+         * 所以两次put操作,虽然操作的是同一个Person实例,但因为中间改变了
+         * name成员变量值,导致在第二次put()时p的hashCode()返回值被改变，
+         * 会被放入Entry[]中的另外一个位置.在get(p)操作时会根据p的hashCode()去对应的
+         * 数组位置处取值,因为此时p的name=“B”,所以取的是第二次的值，但Entry[]数组中存的是
+         * 有2个Entry,即使2个Entry的key是同一个类实例，但在HashMap看来，你的hashCode()不一样，
+         * 你两个key就不一样，这也从侧面说明了正确的重写hashCode()的重要性。
+         */
+        System.out.println(m.get(p)); // Y
+        System.out.println(m.size());// 2
     }
 
     private static class MyObj{
@@ -33,6 +53,19 @@ public class TestHashMap {
         @Override
         public int hashCode() {
             return 15;
+        }
+    }
+
+    private static class Person{
+        private String name;
+
+        @Override
+        public int hashCode() {
+            return name.hashCode();
+        }
+
+        public void setName(String name) {
+            this.name = name;
         }
     }
 }
