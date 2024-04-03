62aad17987478dbf21a8ba0e83105f690db327f0
王宏 <hong.wang8@amh-group.com>
2024-04-03 10:07:56 +0800

    QuickSort

--- src/main/java/com/hong/sort/QuickSort.java
+++ src/main/java/com/hong/sort/QuickSort.java
@@ -2,10 +2,25 @@ package com.hong.sort;
 
 /**
  * <br>快速排序</br>
- *
+ * <p>
  * 快速排序是不稳定的算法
  */
 public class QuickSort {
+
+    /**
+     * 递归使用快速排序,对arr[l...r]的范围进行排序
+     *
+     * @param arr
+     */
+    public static void sort(int[] arr, int l, int r) {
+        if (l >= r) {
+            return;
+        }
+
+        int p = partition2(arr, l, r);
+        sort(arr, l, p - 1);
+        sort(arr, p + 1, r);
+    }
 
     /**
      * 分区.对arr[l...r]部分进行partition操作
@@ -16,20 +31,20 @@ public class QuickSort {
      * @param r
      * @return
      */
-    public static int partition(int[] arr, int l, int r) {
-        int v = arr[l]; // 将第一个元素值作为临界点
-        //arr[l+1...j] < v ; arr[j+1...i) > v
+    public static int partition2(int[] arr, int l, int r) {
+        // 将第一个元素值作为临界点
+        int v = arr[l];
         int j = l;
-        for (int i = l; i <= r; i++) {
-            if (arr[i] < v){
+        for (int i = l + 1; i <= r; i++) {
+            if (arr[i] < v) {
                 /**
                  * 从第一个元素开始依次与临界点值比较,若发现 arr[i]< 临界点,
                  * 则j向前一位,则arr[j]就是当前第一个>v的值,交换i,j.
                  */
-                j++;
-                swap(arr,i,j);
+                swap(arr, i, ++j);
             }
         }
+        //arr[l+1...j] < v < arr[j+1...i)
         swap(arr, l, j);
         return j;
     }
@@ -40,35 +55,11 @@ public class QuickSort {
         arr[j] = t;
     }
 
-    public static int partition2(int[] arr, int l, int r) {
-        int v = arr[l];
-        int j = l;
-        for (int i = l + 1; i <= r; i++) {
-            if (arr[i] < v){
-                swap(arr,i,++j);
-            }
-        }
-        swap(arr, l, j);
-        return j;
-    }
     public static void sort(int[] arr) {
         int n = arr.length;
-        sort(arr, 0, n-1);
+        sort(arr, 0, n - 1);
     }
 
-    /**
-     * 递归使用快速排序,对arr[l...r]的范围进行排序
-     * @param arr
-     */
-    public static void sort(int[] arr, int l, int r) {
-        if( l >= r ){
-            return;
-        }
-
-        int p = partition2(arr, l, r);
-        sort(arr, l, p-1 );
-        sort(arr, p+1, r);
-    }
 
     public static void main(String[] args) {
         // Quick Sort也是一个O(nlogn)复杂度的算法
